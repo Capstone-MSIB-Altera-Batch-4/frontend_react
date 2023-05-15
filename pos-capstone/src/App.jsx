98c108758e5e3ef7d74ebabd8730a716cd9a4e91
AlfinhiHD <alfinhihd@gmail.com>
2023-05-15 13:45:24 +0700

    add bootstrap

--- pos-capstone/src/App.jsx
+++ pos-capstone/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 function App() {
 
 
   return (
     <>
-      
+      <h1>Tes Bootstrap</h1>
     </>
   )
 }
