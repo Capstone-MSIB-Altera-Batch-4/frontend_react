ef155f0089f20b804b5db5df7913681006f5ed36
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2023-05-18 16:47:10 +0700

    slicing our gallery & subscription section

--- pos-capstone/src/App.jsx
+++ pos-capstone/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import OurGallerySection from './component/PageSections/Landing/OurGallerySection';
+import SubscriptionSection from './component/PageSections/Landing/SubscriptionSection';
 
 function App() {
 
 
   return (
     <>
-      <h1>Tes Bootstrap</h1>
+      {/* <h1>Tes Bootstrap</h1> */}
+      <OurGallerySection />
+      <SubscriptionSection />
     </>
   )
 }
