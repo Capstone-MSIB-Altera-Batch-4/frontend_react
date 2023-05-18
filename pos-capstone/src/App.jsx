e29c68c14dcd9b33b6e8cc07a6cc1b159a2c6c93
AlfinhiHD <alfinhihd@gmail.com>
2023-05-19 01:53:04 +0700

    add progress

--- pos-capstone/src/App.jsx
+++ pos-capstone/src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-import TextField from './element/Textfield/Textfield';
+import LoginForm from './component/LoginForm/LoginForm';
 
 function App() {
 
 
   return (
     <>
-      <TextField/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+
+      <div className="container mt-5">
+        <LoginForm />
+      </div>
+      
     </>
+
   )
 }
 
