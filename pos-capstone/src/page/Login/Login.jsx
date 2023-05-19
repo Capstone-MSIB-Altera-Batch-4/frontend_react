2795788fdbc8e6d451d2af887925c0639f9e250a
AlfinhiHD <alfinhihd@gmail.com>
2023-05-19 08:42:13 +0700

    add changes

--- pos-capstone/src/page/Login/Login.jsx
+++ pos-capstone/src/page/Login/Login.jsx
@@ -1,21 +1,10 @@
 import React from "react"
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import "./Login.style.css"
 import Button from "../../element/button/button";
+import LoginForm from "../../component/LoginForm/LoginForm"
 
 const Login = () => {
-    const formik = useFormik({
-        initialValues: {
-            username: "",
-            password: "",
-        },
-        validationSchema: Yup.object().shape({
-            username: Yup.string().required(),
-            password: Yup.string().min(8, "Minimal panjang password 8").required(),
-        }),
 
-    });
     return (
 
         <div className="container-fluid">
@@ -36,45 +25,10 @@ const Login = () => {
                         </h2>
                         <p>Login to maximize the features</p>
                     </div>
-                    <form>
-                        <div className="py-2 pt-2 fw-bold">
-                            <label htmlFor="username" className="form-label">
-                                Username
-                            </label>
-                            <input
-                                type="text"
-                                className={`form-control ${formik.errors.username && `is-invalid`} `}
-                                id="username"
-                                placeholder="username"
-                                aria-label="username"
-                                name="username"
-                                value={formik.values.username}
-                                onChange={formik.handleChange}
-                            />
-                            <div className="invalid-feedback">{formik.errors.username}</div>
-                        </div>
-                        <div className="py-2 pt-2 fw-bold">
-                            <label htmlFor="password" className="form-label">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                className={`form-control ${formik.errors.password && `is-invalid`} `}
-                                id="password"
-                                placeholder="password"
-                                name="password"
-                                value={formik.values.password}
-                                onChange={formik.handleChange}
-                            />
-                            <div className="invalid-feedback">{formik.errors.password}</div>
-                        </div>
-                        <a className="btn-showpass">show password</a>
-                        <div className=" d-grid pt-5" >
-                            <Button
-                                btnName="Login"
-                            />
-                        </div>
-                    </form>
+                    <div className="mt-5">
+                        <LoginForm />
+                    </div>
+
                 </div>
             </div>
         </div >
