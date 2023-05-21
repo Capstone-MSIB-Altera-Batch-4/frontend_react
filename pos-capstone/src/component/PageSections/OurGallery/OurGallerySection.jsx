53ed1ae77fec0f892ef0d58073cc2c7ec0941803
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2023-05-21 08:07:02 +0700

    our gallery & subscribe section fix

--- pos-capstone/src/component/PageSections/OurGallery/OurGallerySection.jsx
+++ pos-capstone/src/component/PageSections/OurGallery/OurGallerySection.jsx
@@ -3,14 +3,14 @@ import './OurGallery.style.css'
 
 const OurGallerySection = () => {
   return (
-    <section id='our-gallery-section' className='our-gallery mx-auto mt-5'>
-      <div className='text-title mx-auto'>
+    <section id='our-gallery-section' className='our-gallery'>
+      <div className='text-title'>
         <div className='d-flex justify-content-between my-3'>
-          <h1 className='section-title fw-bold text-white'>Our Gallery</h1>
-          <h1 className='section-title fw-bold text-white'>製品写真</h1>
+          <h1 className='section-title justify-content-start fw-bold text-white'>Our Gallery</h1>
+          <h1 className='section-title justify-content-end fw-bold text-white'>製品写真</h1>
         </div>
       </div>
-      <img src='/src/assets/device-gallery.svg' alt='device-images' className='device-images'/>
+      <img src='/src/assets/device-gallery.svg' alt='device-images' className='device-images mx-5'/>
     </section>
   )
 }
