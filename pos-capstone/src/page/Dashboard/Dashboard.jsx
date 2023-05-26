c71530d45bdadd1d4c78f705d07a4686fcc4c2ae
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2023-05-26 18:27:31 +0700

    add card and edit some styling

--- pos-capstone/src/page/Dashboard/Dashboard.jsx
+++ pos-capstone/src/page/Dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import Sidebar from "../../component/Sidebar/Sidebar";
 import Table from "../../component/Table/Table";
 import { DummyData } from "../../data/DummyData"
 import Graph from '../../element/Graph/Graph';
+import DashboardCard from '../../component/Cards/DashboardCard/DashboardCard';
+// import {creditCard} from "../../assets/icon/credit-card.svg"
 
 const Dashboard = () => {
   return (
@@ -13,12 +15,18 @@ const Dashboard = () => {
         activedashboard="active"
       />
       <div className='col'>
-        <div className='row'>
+        <div className='row container-fluid mx-auto'>
           <div className='col'>
-            <h1>leoni</h1>
-            <Graph />
+            <div className='row gap-4 mb-3'>
+              <DashboardCard title="Total Sales" amount="Rp61.500.000" growth={"+7%"} icon="../../src/assets/icon/credit-card.svg" />
+              <DashboardCard title="Visitors" amount="245" growth={"+3%"} icon="/src/assets/icon/credit-card.svg" />
+              <DashboardCard title="New Member" amount="65" growth={"+3"} icon="/src/assets/icon/credit-card.svg" />
+            </div>
+            <div className='graph-element p-4 items-center'>
+              <Graph />
+            </div>
           </div>
-          <div className='col'>
+          <div className='col-md-4'>
             <CustomerRanking />
           </div>
           <div className='row'>
