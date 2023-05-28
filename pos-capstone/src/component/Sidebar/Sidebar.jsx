e63952be69e78e0e18ed3e012f014d40208ad561
zwolfio <zwolfio.art@gmail.com>
2023-05-28 20:56:14 +0700

    revisi login & sidebar

--- pos-capstone/src/component/Sidebar/Sidebar.jsx
+++ pos-capstone/src/component/Sidebar/Sidebar.jsx
@@ -1,119 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { dashboard, barItems, help, logout } from "./SidebarItem";
 import "./Sidebar.style.css"
 
-const Sidebar = (props) => {
+const Sidebar = () => {
 
     return (
-            
-                <div className="sidebar bar col auto col-md-2 min-vh-100">
-                    <div className="text-center py-1 ">
-                        <img
-                            className="logo-sidebar p-3"
-                            src="../../src/assets/icon/logo-sidebar.png"
-                            width={150} height={150}
-                        />
-                    </div>
-                    <ul className="nav flex-column">
-                        <li className="nav-item py-3">
-                            <a href="/Dashboard" className={`nav-link ${props.activedashboard}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activedashboard ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-dashboard.svg"}
-                                />
-                                <span className="ms-3">Dashboard</span>
-                            </a>
-                        </li>
-                        <p className="pt-3">MANAGEMENT</p>
-                        <li className="nav-item pb-2">
-                            <a href="/Product" className={`nav-link ${props.activeproducts}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeproducts ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-products.svg"}
-                                />
-                                <span className="ms-3">Products</span>
-                                        
-                            </a>
-                        </li>
-                        <li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activemember}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activemember ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-member.svg"}
-                                />
-                                <span className="ms-3">Memberships</span>
-                            </a>
-                        </li>
-                        <li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activeorder}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeorder ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-order.svg"}
-                                />
-                                <span className="ms-3">Orders</span>
-                            </a>
-                        </li>
-                        <li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activeinvoices}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeinvoices ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-invoices.svg"}
-                                />
-                                <span className="ms-3">Invoices</span>
-                            </a>
-                        </li>
-                        <li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activecashier}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeinvoices ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-cashier.svg"}
-                                />
-                                <span className="ms-3">Cashier</span>
-                            </a>
-                        </li>
-                        <hr />
-                        <li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activecashier}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeinvoices ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-help.svg"}
-                                />
-                                <span className="ms-3">Help</span>
-                            </a>
-                        </li><li className="nav-item pb-2">
-                            <a href="" className={`nav-link ${props.activecashier}`}>
-                                <img
-                                    className="icon mb-1 ms-1"
-                                    src={props.activeinvoices ?
-                                        "../../src/assets/icon/logo-dashboard.svg" :
-                                        "../../src/assets/icon/icon-black/logo-logout.svg"}
-                                />
-                                <span className="ms-3">Logout</span>
-                            </a>
-                        </li>
-                        <p className="branch mt-5">
-                            Branch Location:
-                            <br/>
-                            Jl. Asia Afrika No.19, RT.1/RW.3, Gelora, 
-                            Kecamatan Tanah Abang, Kota Jakarta Pusat, 
-                            Daerah Khusus Ibukota Jakarta 10270
-                        </p>
-                    </ul>
-                </div>
+        <div className="sidebar bar col auto col-md-2 min-vh-100">
+            <div className="text-center mt-3 py-1 ">
+                <img
+                    className="logo-sidebar p-3"
+                    src="../../src/assets/icon/logo-sidebar.png"
+                    width={180} height={180}
+                />
+            </div>
+            <ul className="nav flex-column">
+                <li>
+                    <NavLink to={dashboard.path} className="nav-link mt-3" activeclassName="active" >
+                        <div className="icon">{dashboard.icon}</div>
+                        <div className="barname">{dashboard.name}</div>
+                    </NavLink>
+                </li>
+                <p className="pt-4">MANAGEMENT</p>
+                {barItems.map((item, idx) =>
+                    <li>
+                        <NavLink to={item.path} key={idx} className="nav-link" activeclassName="active" >
+                            <div className="icon">{item.icon}</div>
+                            <div className="barname">{item.name}</div>
+                        </NavLink>
+                    </li>
+                )}
+                <hr/>
+                <li>
+                    <NavLink to={help.path} className="nav-link" activeclassName="active" >
+                        <div className="icon">{help.icon}</div>
+                        <div className="barname">{help.name}</div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={help.path} className="nav-link" activeclassName="active" >
+                        <div className="icon">{logout.icon}</div>
+                        <div className="barname">{logout.name}</div>
+                    </NavLink>
+                </li>
+                <p className="branch mt-5">
+                    Branch Location:
+                    <br />
+                    Jl. Asia Afrika No.19, RT.1/RW.3, Gelora,
+                    Kecamatan Tanah Abang, Kota Jakarta Pusat,
+                    Daerah Khusus Ibukota Jakarta 10270
+                </p>
+            </ul>
+        </div>
     )
 
 
