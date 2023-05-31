ebb993e11cf39a7a29e68f65c6c84870ef1ce9ad
AlfinhiHD <alfinhihd@gmail.com>
2023-05-31 19:33:15 +0700

    add simple changes

--- pos-capstone/src/page/Dashboard/Products.jsx
+++ pos-capstone/src/page/Dashboard/Products.jsx
@@ -4,11 +4,10 @@ import PrimaryButton from "../../element/Button/PrimaryButton/PrimaryButton"
 const Products = () => {
 
     return (
-     <>
-     <SecondaryButton label="Ini secondary" />
-     <PrimaryButton label="Ini primary" />
-     
-     </>
+        <>
+            {/* <SecondaryButton label="Ini secondary" />
+                <PrimaryButton label="Ini primary" /> */}
+        </>
     )
 
 }
