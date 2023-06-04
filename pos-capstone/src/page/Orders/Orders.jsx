2d6bde624d1b1147a6195b3591e33047ca72ce58
zwolfio <zwolfio.art@gmail.com>
2023-06-04 08:23:42 +0700

    add inputdate

--- pos-capstone/src/page/Orders/Orders.jsx
+++ pos-capstone/src/page/Orders/Orders.jsx
@@ -1,10 +1,16 @@
 import TestingTable from "../../component/Table/TestingTable";
 import TableEditDelete from "../../data/DummyData";
+import InputDate from "../../element/InputDate/InputDate";
 
 const Orders = () => {
   return (
       <div className="default-products col">
         <TestingTable />
+        <InputDate
+          className="inputdate"
+          placeholder="From"
+          
+        />
       </div>
   );
 };
